4f7959dba77545c91b10b0cc6ac3be03c0d16e8d
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-04-10 11:56:40 -0400

    Update tilemap-editor.js

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -103,7 +103,7 @@
         </div>
         
         <div>
-            <div id="toolButtonsWrapper" class="tool_wrapper"> <text>Tools | </text>
+            <div id="toolButtonsWrapper" class="tool_wrapper"> <label>Tools: </label>
               <input id="tool0" type="radio" value="0" name="tool" checked class="hidden"/>
               <label for="tool0" title="paint tiles" data-value="0" class="menu">
                   <div style="text-decoration: underline; text-underline-offset: 7px;" id="flipBrushIndicator">✏️</div>
