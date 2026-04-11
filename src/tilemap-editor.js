8fccb9624de038e895d396a3a0372fe3f166e22a
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-04-11 11:05:45 -0400

    Update tilemap-editor.js

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -268,7 +268,7 @@
 <summary title="press the arrow to show / hide map & layer features"> |
       
       <div id="mapSelectContainer" class="tilemaps_selector">
-            <select name="mapsData" id="mapsDataSel"></select>
+            <small><select name="mapsData" id="mapsDataSel"></select></small>
             <button id="addMapBtn" title="Add tilemap">+</button>
             <button id="removeMapBtn" title="Remove tilemap">-</button>        
             <button id="duplicateMapBtn" title="Clone Copy map"><b>⧉</b></button>     
