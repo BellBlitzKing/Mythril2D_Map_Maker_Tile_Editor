5b450b5b98c210707e442d53e08b285de21737bf
BellBlitzKing <40621950+BellBlitzKing@users.noreply.github.com>
2026-04-14 21:32:30 -0400

    Change slider width to 120px and ht to 5px

--- src/tilemap-editor.js
+++ src/tilemap-editor.js
@@ -401,7 +401,7 @@
                     <div style="font-size: small;" class="slider-wrapper">
                       <label title="Opacity change for layer transparency" for="layerOpacitySlider">    🥃   Layer Clear Opacity</label><br>
                       <text>  &hairsp;&hairsp;🪟</text>
-                      <input style="height: 5px;" type="range" min="0" max="1" value="1" id="layerOpacitySlider" step="0.01">
+                      <input style="width: 120px; height: 5px;" type="range" min="0" max="1" value="1" id="layerOpacitySlider" step="0.01">
                       <output for="layerOpacitySlider" id="layerOpacitySliderValue">${maps[ACTIVE_MAP].layers[newLayer]?.opacity}</output>
                     </div>
                     </small>
